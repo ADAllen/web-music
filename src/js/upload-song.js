e5577b9f4a27291ba1e216a22446a9f213d4913f
ADAllen <1007715170@qq.com>
2019-03-04 19:02:22 +0800

    mvc

---
+++ src/js/upload-song.js
@@ -0,0 +1,30 @@
+
+
+
+
+{
+    let view={
+        el:'.songUpload',
+        
+        
+        
+    }
+    let model={}
+    let controller={
+        init(view,model){
+            this.view=view
+            this.model=model
+        },
+        
+          
+
+            
+        
+    
+  
+
+
+
+
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-Bmob.initialize("6a3b16d9a49aadb7205c231e0931ed57", "848173d2ba42cc9a8ea47b3c03dabedc");
-const query = Bmob.Query('songs');
+Bmob.initialize("6a3b16d9a49aadb7205c231e0931ed57", "848173d2ba42cc9a8ea47b3c03dabedc")
+const query = Bmob.Query('songs')
 query.set("SongName","Bmob")
 query.set("singer","爱谁谁")
 query.save().then(res => {
@@ -7,19 +7,21 @@ query.save().then(res => {
 }).catch(err => {
   console.log(err)
 })
-const fileUploadControl = document.getElementById('fileUpload');
-fileUploadControl.onchange = () => {
-  const pic = fileUploadControl.files
-  let file
-  for(let item of pic){
-     file = Bmob.File(item.name, item);
-  }
-  file.save().then(res => {
-    console.log(res.length);
+const fileUploadControl = document.getElementById('fileUpload')
+            fileUploadControl.onchange = () => {
+                const pic = fileUploadControl.files
+                let file
+                for(let item of pic){
+                file = Bmob.File(item.name, item);
+            }
+                file.save().then(res => {
+                    console.log(res.length);
     
-    console.log(res);
-    console.log(res[0].url);
+                    console.log(res);
+                    console.log(res[0].url);
 
   
-  })
-}
+                })
+            }
+    
+
